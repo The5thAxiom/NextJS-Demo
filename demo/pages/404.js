530500ce18ede076966c7d43cd7ec8e2d1fe7917
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-07 23:03:59 +0530

    added custom 404 page

---
+++ demo/pages/404.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main>
+            <h1>404: Content Not Found</h1>
+            <p>Please check if the URL you entered is correct.</p>
+        </main>
+    );
+}
