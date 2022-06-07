b90d416a7f50d009d99739eb753df7b901737c59
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-07 17:57:49 +0530

    moved nav and footer to _app file

--- demo/pages/_app.js
+++ demo/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
+
--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,16 +1,9 @@
-import Footer from '../components/footer';
-import Navbar from '../components/navbar';
-
 export default function Home() {
     return (
-        <>
-            <Navbar />
-            <main>
-                <h1>Hello!</h1>
-                <p>This is samridh!</p>
-            </main>
-            <Footer />
-        </>
+        <main>
+            <h1>Hello!</h1>
+            <p>This is samridh!</p>
+        </main>
     );
 }
 
