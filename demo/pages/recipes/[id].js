df9e7ad26232dac8d37696548016aee7ed0cb41f
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-08 00:00:58 +0530

    individual recipe page is SSR (browse one is SSG not SSR)

--- demo/pages/recipes/[id].js
+++ demo/pages/recipes/[id].js
@@ -1,36 +1,54 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
-import Loading from '../../components/loading';
 
-export default function Recipes() {
-    const router = useRouter();
-    const { id } = router.query;
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+    const res = await fetch(
+        `https://cookbook-demo-the5thaxiom.herokuapp.com/api/recipes/${id}/full`
+    );
+    const data = await res.json();
+    return { props: { recipe: data } };
+}
 
-    const [recipe, setRecipe] = useState(null);
-
-    useEffect(() => {
-        if (id)
-            fetch(
-                `https://cookbook-demo-the5thaxiom.herokuapp.com/api/recipes/${id}`
-            )
-                .then(res => (res.ok ? res.json() : null))
-                .then(data => setRecipe(data));
-    }, [id]);
-
-    if (recipe)
-        return (
-            <main>
-                <Head>
-                    <title>{recipe.name}</title>
-                </Head>
-                <h1>{recipe.name}</h1>
-            </main>
-        );
-    else
-        return (
-            <main>
-                <Loading />
-            </main>
-        );
+export default function Recipes({ recipe }) {
+    console.log(recipe);
+    return (
+        <main>
+            <Head>
+                <title>{recipe.name}</title>
+            </Head>
+            <h1>{recipe.name}</h1>
+            <br />
+            <b>{recipe.vegetarian ? 'veg' : 'non-veg'}</b> {' | '}
+            <b>{`takes ${recipe.prep_time} minutes`}</b> {' | '}
+            <b>{`makes ${recipe.quantity} ${recipe.unit}`}</b> {' | '}
+            <b>{`difficulty: ${'★'.repeat(recipe.difficulty)}`}</b>
+            <br />
+            <br />
+            <em>{recipe.description}</em>
+            <br />
+            <br />
+            <ul>
+                {recipe.recipe_tags.map(t => (
+                    <li>{t}</li>
+                ))}
+            </ul>
+            <h2>Ingredients</h2>
+            <ol>
+                {recipe.recipe_ingredients.map((ing, index) => (
+                    <li key={index} className='recipe-ingredient'>
+                        {ing.quantity} {ing.unit} of {ing.english_name} (
+                        {ing.hindi_name_devnagari} | {ing.hindi_name_latin})
+                    </li>
+                ))}
+            </ol>
+            <h2>Steps</h2>
+            <ol>
+                {recipe.recipe_steps.map((step, index) => (
+                    <li key={index} className='recipe-step'>
+                        {step}
+                    </li>
+                ))}
+            </ol>
+        </main>
+    );
 }
