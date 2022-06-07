d1e454b5de66d4436845d4b7eeb6bc4a70b0c5eb
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-07 22:42:27 +0530

    worked on styling

--- demo/components/navbar.js
+++ demo/components/navbar.js
@@ -1,11 +1,30 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const links = [
+    { title: 'Home', to: '/' },
+    { title: 'About', to: '/about' },
+    { title: 'Recipes', to: '/recipes' }
+];
 
 export default function Navbar() {
+    const router = useRouter();
+    const activeLinks = links.map(l => {
+        return { ...l, isActive: router.pathname === l.to };
+    });
+    console.log(activeLinks);
     return (
         <nav>
-            <Link href='/'>Home</Link>
-            <Link href='/about'>About</Link>
-            <Link href='/recipes/'>Browse recipes</Link>
+            <div className='logo'>Demo</div>
+            {activeLinks.map((l, i) => (
+                <div
+                    key={i}
+                    // className='link'
+                    className={'link' + (l.isActive ? ' active' : ' inactive')}
+                >
+                    <Link href={l.to}>{l.title}</Link>
+                </div>
+            ))}
         </nav>
     );
 }
